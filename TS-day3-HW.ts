a16236a1a4990a4a255e48efe665dc9fdd19a9ef
9000HAL <gsanchezscs@gmail.com>
2023-06-17 22:52:58 -0700

    wrapping hw for repo link to thieves118

--- TS-day3-HW.ts
+++ TS-day3-HW.ts
@@ -12,7 +12,21 @@ let users = [
     }
 ];
 
+type User = {
+    name: string;
+    age: number;
+    company?: string;
+};
+
+
+
+
 // 2. Define a type for representing the days of week. Valid values are “Monday”, “Tuesday”, etc.
+
+type DayOfWeek = "Monday" | "Tuesday" | "Wednesday" | "Thursday" | "Friday" | "Saturday" | "Sunday";
+
+
+
 
 // 3. Given the Person class below, create a getter for getting the full name of a person.
 
@@ -22,17 +36,36 @@ class Person {
         public lastName:string) {}
 }
 
+
+
+
 // 4. Create a new class called "Employee" that extends "Person" and adds a new property called salary. 
+//NOTE: SKIP THIS QUESTION #4 PER C. ASKEW ******************************************************
+
+
 
 
 // 5. Given the data below, define an interface for representing employees:
 // HINT: You'll need 2 interfaces.
-let employee = {
-    name:'Christian Askew',
-    salary:1_000_000,
-    address:{
-        street:'Thieves st',
-        city:'Seattle',
+
+interface Address {
+    street: string;
+    city: string;
+    zipCode: number;
+}
+
+interface Employee {
+    name: string;
+    salary: number;
+    address: Address;
+}
+
+let employee: Employee = {
+    name: 'Christian Askew',
+    salary: 1_000_000,
+    address: {
+        street: 'Thieves st',
+        city: 'Seattle',
         zipCode: 98101
     }
 };
